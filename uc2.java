964229c47445049e8eff8d79d86c4e3a46609233
saurabh <dekapurwars@gmail.com>
2021-08-23 23:17:14 +0530

    rolling a die

---
+++ uc2.java
@@ -0,0 +1,10 @@
+
+public class uc2 {
+public static void main(String[] args) {
+		
+		int check =(int) Math.floor(Math.random() * 10) %6 +1;
+		
+		System.out.println("Current position is :" + check);	
+
+}
+}
