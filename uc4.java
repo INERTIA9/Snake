0c9b38e3c038cf82fae3ed3d208dbc1503081599
saurabh <dekapurwars@gmail.com>
2021-08-23 23:37:36 +0530

    reaching 100th position

---
+++ uc4.java
@@ -0,0 +1,24 @@
+
+public class uc4 {
+	public static void main(String[] args) {
+		int position=0;
+		while(position<=100) {
+		int snake= (int) Math.floor(Math.random() * 10) %2;
+		int check =(int) Math.floor(Math.random() * 10) %6 +1;
+		if(snake==1) {
+			position =position + check;
+		}
+		else if(snake==1)  {
+			position =position - check;
+			if(position<0) {
+				position=0;
+			}
+		}
+		else  {
+			position =position;
+			
+		}
+		System.out.println("current postion  is: " + position);
+}
+	}
+}
