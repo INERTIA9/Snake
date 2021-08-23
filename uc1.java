9afd502372e952a2e45c2a613755757a243ee21d
Saurabh Dekapurwar <88469914+INERTIA9@users.noreply.github.com>
2021-08-24 00:22:52 +0530

    Update uc1.java

--- uc1.java
+++ uc1.java
@@ -1,4 +1,9 @@
 
 public class uc1 {
-
+public static void main(String[] args) {
+		int position=0;
+		int check =(int) Math.floor(Math.random() * 10) %6 +1;
+		position=position + check;
+		System.out.println("Current position is :" + position);
+}
 }
